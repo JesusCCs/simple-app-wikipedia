a623b423431784ec194ba84becad5d8f853ddfd2
Jesús Cañas Cobos <jesus.c.cobos@gmail.com>
2021-02-12 16:36:53 +0100

    Solucionado problemas de scroll infinito

--- src/components/molecules/Listado.js
+++ src/components/molecules/Listado.js
@@ -6,20 +6,16 @@ import Loading from '../atoms/Loading';
 
 export default function Listado({busqueda, navigation}) {
   const [data, setData] = React.useState();
-  const [offSet, setOffSet] = React.useState(0);
-  const [ID, setID] = React.useState(0);
+  const [offSet, setOffSet] = React.useState(10);
 
   const [isLoading, setLoading] = React.useState(true);
   const [refreshing, setRefreshing] = React.useState(false);
   const [refreshIndicator, setRefreshIndicator] = React.useState(false);
 
   React.useEffect(() => {
-    let IDEffect = 0;
     getBusqueda(busqueda).then((lista) => {
-      lista.forEach((el) => (el.id = ++IDEffect));
       setData(lista);
       setLoading(false);
-      setID(IDEffect);
     });
   }, [busqueda]);
 
@@ -30,25 +26,19 @@ export default function Listado({busqueda, navigation}) {
     setOffSet(offSet + 10);
     setRefreshing(true);
 
-    let IDEffect = ID;
     getBusqueda(busqueda, offSet).then((lista) => {
-      lista.forEach((el) => (el.id = ++IDEffect));
       setData(data.concat(lista));
       setRefreshing(false);
-      setID(IDEffect);
     });
   };
 
   const onRefresh = async () => {
     setRefreshIndicator(true);
-    setOffSet(0);
+    setOffSet(10);
 
-    let IDEffect = ID;
     getBusqueda(busqueda).then((lista) => {
-      lista.forEach((el) => (el.id = ++IDEffect));
       setData(lista);
       setRefreshIndicator(false);
-      setID(IDEffect);
     });
   };
 
@@ -57,7 +47,7 @@ export default function Listado({busqueda, navigation}) {
   ) : (
     <FlatList
       data={data}
-      keyExtractor={({id}) => id.toString()}
+      keyExtractor={({pageid}) => pageid.toString()}
       initialNumToRender={10}
       onEndReachedThreshold={0.5}
       onEndReached={onEndReached}
